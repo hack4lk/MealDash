f2786821a418c8c09d35c64444397d3767e34c6c
Marco Lozano <marcolozano@MacBook-Pro-de-Marco.local>
2025-07-19 18:43:22 -0500

    Added cors on backend, added integration of query

--- server/api/app.js
+++ server/api/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const forecastRoutes = require('./routes/forecastRoutes');
 
+app.use(cors());
 app.use(express.json());
 app.use('/', forecastRoutes);
 
